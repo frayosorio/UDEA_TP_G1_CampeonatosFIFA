378cb1e313f2e0a49d8d25e59d719254fe30099f
unknown <frayosorio@gmail.com>
2026-04-29 21:37:35 -0500

    Se incluyen instrucciones idempotentes para agregar paises a los grupos del campeonato FIFA 2026 y agregar ciudades sede

--- Consultas - Poblar Mundial 2026 - CAMPEONATOS FIFA.sql
+++ Consultas - Poblar Mundial 2026 - CAMPEONATOS FIFA.sql
@@ -48,22 +48,77 @@ SELECT * FROM pais
 	'España',
 	'Suecia',
 	'Suiza',
-	'Turquía');
+	'Turquía')
+	ORDER BY pais;
 
 --Listar paises organizadores del campeonato
-SELECT campeonato, año, STRING_AGG(pais, ', ') paisesorganizadores
+SELECT C.id, campeonato, año, STRING_AGG(pais, ', ') paisesorganizadores
 	FROM campeonato C
 		JOIN campeonatopais CP ON C.id = CP.idcampeonato
 		JOIN pais PO ON PO.id = CP.idpais
 	WHERE C.campeonato = 'FIFA World Cup 2026'
-	GROUP BY campeonato, año
+	GROUP BY C.id, campeonato, año
 
 
 --Listar campeonato con los grupos y los paises
-SELECT campeonato, año
+SELECT campeonato, año, grupo, G.id, pais, P.id, P.entidad
 	FROM campeonato C
-		JOIN grupo G ON G.idcampeonato = C.id
+		LEFT JOIN grupo G ON G.idcampeonato = C.id
+		LEFT JOIN grupopais GP ON GP.idgrupo = G.id
+		LEFT JOIN pais P ON P.id = GP.idpais
 	WHERE C.campeonato = 'FIFA World Cup 2026'
-	GROUP BY campeonato, año
+
+SELECT campeonato, año, grupo, G.id, STRING_AGG(pais, ', ') paises
+	FROM campeonato C
+		LEFT JOIN grupo G ON G.idcampeonato = C.id
+		LEFT JOIN grupopais GP ON GP.idgrupo = G.id
+		LEFT JOIN pais P ON P.id = GP.idpais
+	WHERE C.campeonato = 'FIFA World Cup 2026'
+	GROUP BY campeonato, año, grupo, G.id
+
+--Listar los grupos llamados 'A'
+SELECT * FROM grupo
+	WHERE grupo='A'
+
+--Listar los estadios y sus ciudades
+SELECT P.pais, C.ciudad, E.estadio, E.capacidad
+	FROM pais P
+		JOIN ciudad C ON P.id = C.idpais
+		LEFT JOIN estadio E ON E.idciudad = C.id
+	WHERE P.pais IN ('México', 'Estados Unidos', 'Canadá')
 	
+
+--Eliminar todos los paises de los grupos del campeonato con ID=12 (Campoenato Mundial 2026)
+DELETE FROM grupopais GP
+	USING grupo G
+	WHERE GP.idgrupo = G.id
+	  AND G.idcampeonato = 12;
+
+--Eliminar todos los grupos del campeonato con ID=12 (Campeonato Mundial 2026)
+DELETE FROM grupo
+	WHERE idcampeonato=12
+
+--Eliminar los encuentros jugados en los estadios y ciudades de los paises organizadores (Campeonato Mundial 2026)
+DELETE FROM encuentro EN
+	USING estadio ES
+		JOIN ciudad C ON C.id = ES.idciudad
+		JOIN pais P ON C.idpais = P.id
+	WHERE EN.idestadio= ES.id
+	  AND P.pais IN ('México', 'Estados Unidos', 'Canadá')
+
+
+--Eliminar los estadios de ciudades de los paises organizadores (Campeonato Mundial 2026)
+DELETE FROM estadio E
+	USING ciudad C
+		JOIN pais P ON C.idpais = P.id
+	WHERE E.idciudad = C.id
+	  AND P.pais IN ('México', 'Estados Unidos', 'Canadá')
+
+--Eliminar las ciudades de los paises organizadores (Campeonato Mundial 2026)
+DELETE FROM ciudad C
+	USING pais P
+	WHERE C.idpais = P.id
+	  AND P.pais IN ('México', 'Estados Unidos', 'Canadá')
+
+
 	
--- Poblar Campeonato 2026 - CAMPENATOS FIFA.sql
+++ Poblar Campeonato 2026 - CAMPENATOS FIFA.sql
@@ -6,20 +6,30 @@ SELECT setval(
     (SELECT MAX(id) FROM pais)
 	);
 
+SELECT setval(
+    pg_get_serial_sequence('campeonato', 'id'),
+    (SELECT MAX(id) FROM campeonato)
+	);
+
+SELECT setval(
+    pg_get_serial_sequence('grupo', 'id'),
+    (SELECT MAX(id) FROM grupo)
+	);
+
 --1. Actualización de todos los paises
 
 INSERT INTO pais
 	(pais, entidad)
-	VALUES 	('Australia', ''),
-	('Irán', ''),
-	('Irak', ''),
-	('Japón', ''),
-	('Jordania', ''),
+	VALUES 	('Australia', 'Football Australia'),
+	('Irán', 'Federación de Fútbol de Irán'),
+	('Irak', 'Federación Iraquí de Fútbol'),
+	('Japón', 'Asociación de Fútbol de Japón'),
+	('Jordania', 'Federación de Fútbol de Jordania'),
 	('Catar', ''),
 	('Arabia Saudita', ''),
 	('Corea del Sur', ''),
 	('Uzbekistán', ''),
-	('Argelia', ''),
+	('Argelia', 'Federación Argelina de Fútbol'),
 	('Cabo Verde', ''),
 	('República Democrática del Congo', ''),
 	('Egipto', ''),
@@ -58,13 +68,18 @@ INSERT INTO pais
 	('Suecia', ''),
 	('Suiza', ''),
 	('Turquía', '')
-	ON CONFLICT(pais) DO NOTHING ;
+	ON CONFLICT(pais) 
+	DO UPDATE
+		SET entidad = excluded.entidad
+		WHERE excluded.entidad<>'';
 
 --2. Actualización del Campeonato
 INSERT INTO campeonato
 	(campeonato, año)
 	VALUES('FIFA World Cup 2026', 2026)
-	ON CONFLICT(campeonato) DO NOTHING ;
+	ON CONFLICT(campeonato) 
+	DO UPDATE
+		SET año = excluded.año;
 
 --3. Actualización de los Paises organizadores del Campeonato
 INSERT INTO campeonatopais
@@ -88,4 +103,104 @@ INSERT INTO grupo
 			ON C.campeonato = 'FIFA World Cup 2026'
 	ON CONFLICT(idcampeonato, grupo) DO NOTHING;
 	
-	
+--5. Actualización de los paises en los grupos
+INSERT INTO grupopais
+	(idgrupo, idpais)
+	SELECT G.id, P.id
+		FROM grupo G
+			JOIN campeonato C
+				ON C.id = G.idcampeonato AND C.campeonato = 'FIFA World Cup 2026'
+			JOIN (
+				VALUES
+				('México', 'A'),
+				('Sudáfrica', 'A'),
+				('Corea del Sur','A'),
+				('República Checa','A'),
+		
+				('Canadá','B'),
+				('Suiza','B'),
+				('Catar','B'),
+				('Bosnia y Herzegovina','B'),
+		
+				('Brasil','C'),
+				('Marruecos','C'),
+				('Escocia','C'),
+				('Haití','C'),
+		
+				('Estados Unidos','D'),
+				('Paraguay','D'),
+				('Australia','D'),
+				('Turquía','D'),
+		
+				('Alemania','E'),
+				('Ecuador','E'),
+				('Costa de Marfil','E'),
+				('Curaçao','E'),
+		
+				('Países Bajos','F'),
+				('Japón','F'),
+				('Suecia','F'),
+				('Túnez','F'),
+		
+				('Bélgica','G'),
+				('Egipto','G'),
+				('Irán','G'),
+				('Nueva Zelanda','G'),
+		
+				('España','H'),
+				('Uruguay','H'),
+				('Arabia Saudita','H'),
+				('Cabo Verde','H'),
+		
+				('Francia','I'),
+				('Senegal','I'),
+				('Noruega','I'),
+				('Irak','I'),
+		
+				('Argentina','J'),
+				('Argelia','J'),
+				('Austria','J'),
+				('Jordania','J'),
+		
+				('Portugal','K'),
+				('República Democrática del Congo','K'),
+				('Uzbekistán','K'),
+				('Colombia','K'),
+		
+				('Inglaterra','L'),
+				('Croacia','L'),
+				('Ghana','L'),
+				('Panamá','L')
+				) datos(pais, grupo)
+				ON G.grupo = datos.grupo
+			JOIN pais P
+				ON datos.pais = P.pais
+	ON CONFLICT(idgrupo, idpais) DO NOTHING;
+
+--6. Actualización de Ciudades
+INSERT INTO ciudad
+	(ciudad, idpais)
+	SELECT datos.ciudad, P.id
+		FROM (
+				VALUES
+				('México D.F.','México'),
+				('Guadalajara','México'),
+				('Monterrey','México'),
+				('Atlanta','Estados Unidos'),
+				('Boston','Estados Unidos'),
+				('Dallas','Estados Unidos'),
+				('Houston','Estados Unidos'),
+				('Kansas City','Estados Unidos'),
+				('Los Ángeles','Estados Unidos'),
+				('Miami','Estados Unidos'),
+				('Nueva Jersey','Estados Unidos'),
+				('Filadelfia','Estados Unidos'),
+				('San Francisco','Estados Unidos'),
+				('Seattle','Estados Unidos'),
+				('Toronto','Canadá'),
+				('Vancouver','Canadá')
+				) datos(ciudad, pais)
+			JOIN pais P
+				ON P.pais = datos.pais
+	ON CONFLICT(ciudad, idpais) DO NOTHING
+				
